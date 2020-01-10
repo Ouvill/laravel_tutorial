fbcd90011386875937db29b12bcf664cdea5b372
Ouvill <contact@ouvill.net>
2020-01-10 17:22:19 +0900

    status class を追加

--- app/Task.php
+++ app/Task.php
@@ -6,11 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
     const STATUS = [
-        1 => ["label" => "未着手"],
-        2 => ["label" => "着手中"],
-        3 => ["label" => "完了"],
+        1 => ['label' => '未着手', 'class' => 'label-danger'],
+        2 => ['label' => '着手中', 'class' => 'label-info'],
+        3 => ['label' => '完了', 'class' => ''],
     ];
 
     /**
@@ -28,5 +27,22 @@ class Task extends Model
         }
 
         return self::STATUS[$status]['label'];
+    }
+
+    /**
+     * 状態を表すHTMLクラス
+     * @return string
+     */
+    public function getStatusClassAttribute()
+    {
+        // 状態値
+        $status = $this->attributes['status'];
+
+        // 定義されていなければ空文字を返す
+        if (!isset(self::STATUS[$status])) {
+            return '';
+        }
+
+        return self::STATUS[$status]['class'];
     }
 }
