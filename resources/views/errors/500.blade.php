8dc1bb581cc84f211740dfc23dfa5e4cf4fd20a2
Ouvill <contact@ouvill.net>
2020-01-14 19:24:34 +0900

    エラーメッセージのカスタマイズ

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,14 @@
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col col-md-offset-3 col-md-6">
+            <div class="text-center">
+                <p>ただ今問題が発生しており、表示できません</p>
+                <a href="{{ route('home') }}" class="btn">
+                    ホームへ戻る
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
